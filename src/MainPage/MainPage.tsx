062db73ecd8f7aaff4b4c13891789fea5ce28e54
Robin Wiersig <Opsi@users.noreply.github.com>
2022-12-10 03:46:20 +0100

    add eslint and prettier

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,32 +1,32 @@
-import { useStore } from "../store/store";
+import { useStore } from '../store/store';
 
 const Ingredients = () => {
-	const ingredients = useStore((state) => state.ingredients);
-	const addIngredients = useStore((state) => state.addIngredients);
-	return (
-		<div>
-			<button onClick={() => addIngredients(1)}>Add Ingredient</button>
-			<div>Ingredients: {ingredients}</div>
-		</div>
-	);
+  const ingredients = useStore((state) => state.ingredients);
+  const addIngredients = useStore((state) => state.addIngredients);
+  return (
+    <div>
+      <button onClick={() => addIngredients(1)}>Add Ingredient</button>
+      <div>Ingredients: {ingredients}</div>
+    </div>
+  );
 };
 
 const RareIngredients = () => {
-	const rareIngredients = useStore((state) => state.rareIngredients);
-	const addRareIngredients = useStore((state) => state.addRareIngredients);
-	return (
-		<div>
-			<button onClick={() => addRareIngredients(1)}>Add Rare Ingredient</button>
-			<div>Rare Ingredients: {rareIngredients}</div>
-		</div>
-	);
+  const rareIngredients = useStore((state) => state.rareIngredients);
+  const addRareIngredients = useStore((state) => state.addRareIngredients);
+  return (
+    <div>
+      <button onClick={() => addRareIngredients(1)}>Add Rare Ingredient</button>
+      <div>Rare Ingredients: {rareIngredients}</div>
+    </div>
+  );
 };
 
 export const MainPage: React.FC = () => {
-	return (
-		<div className="text-2xl font-bold underline">
-			<Ingredients />
-			<RareIngredients />
-		</div>
-	);
+  return (
+    <div className="text-2xl font-bold underline">
+      <Ingredients />
+      <RareIngredients />
+    </div>
+  );
 };
